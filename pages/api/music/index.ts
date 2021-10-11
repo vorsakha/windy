f64e7256ada776b949dde1439ee75910b1abdc51
vorsakha <andreitf.dev@gmail.com>
2021-10-11 19:31:49 -0300

    change api source for music route

--- pages/api/music/index.ts
+++ pages/api/music/index.ts
@@ -2,24 +2,24 @@ import axios from "axios";
 import { NextApiResponse } from "next";
 import { NextApiRequest } from "next";
 
-const apiKey = process.env.MUSIC_API_KEY;
+const apiKey = process.env.MUSIC_API_KEY || "";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const {
-    method,
-    query: { id },
-  } = req;
+  const { method } = req;
 
   switch (method) {
     case "GET":
       try {
+        const options = {
+          headers: {
+            "x-rapidapi-host": "shazam.p.rapidapi.com",
+            "x-rapidapi-key": apiKey,
+          },
+        };
+
         const response = await axios.get(
-          `https://api.spotify.com/v1/browse/categories/${id}/playlists?country=BR&offset=0&limit=5`,
-          {
-            headers: {
-              Authorization: "Bearer " + apiKey,
-            },
-          }
+          "https://shazam.p.rapidapi.com/charts/list",
+          options
         );
 
         res.status(200).send(response.data);
