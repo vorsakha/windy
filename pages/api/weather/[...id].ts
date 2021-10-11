39ea5fcbda31ff2d66c9c7d8c8e9e140c0ca89e3
vorsakha <andreitf.dev@gmail.com>
2021-10-11 16:46:50 -0300

    api/weather - add weather logic

--- pages/api/weather/[...id].ts
+++ pages/api/weather/[...id].ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { NextApiResponse } from "next";
-import { WEATHER_API_KEY } from "../../../constants";
+
+const apiKey = process.env.WEATHER_API_KEY;
 
 export default async (req: NextRequest, res: NextApiResponse) => {
   const {
@@ -14,10 +15,37 @@ export default async (req: NextRequest, res: NextApiResponse) => {
     case "GET":
       try {
         const response = await axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
         );
 
-        res.send(response.data);
+        const responseData: {
+          main: { temp: number };
+          name: string;
+          sys: { country: string };
+        } = response.data;
+
+        const temp = responseData.main.temp;
+        const country = responseData.sys.country;
+        const city = responseData.name;
+
+        const resObject = {
+          temperature: temp,
+          country: country,
+          city: city,
+          playlistType: "Metal",
+        };
+
+        if (temp > 32) {
+          resObject.playlistType = "Rock";
+        } else if (temp < 32 && temp > 24) {
+          resObject.playlistType = "Pop";
+        } else if (temp < 24 && temp > 16) {
+          resObject.playlistType = "Classica";
+        } else if (temp < 16) {
+          resObject.playlistType = "Lofi";
+        }
+
+        res.status(200).send(resObject);
       } catch (error: any) {
         return res.status(400).send(error.message);
       }
