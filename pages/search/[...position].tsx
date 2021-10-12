35bd6ee87323a22d4bbf0656b09b6869672ff725
vorsakha <andreitf.dev@gmail.com>
2021-10-12 18:20:51 -0300

    search api route

---
+++ pages/search/[...position].tsx
@@ -0,0 +1,39 @@
+import Head from "next/head";
+import React from "react";
+import Banner from "../../components/Banner";
+import SongList from "../../components/SongList";
+import useParams from "../../hooks/useParams";
+import useSongList from "../../hooks/useSongList";
+import useWeather from "../../hooks/useWeather";
+
+const Search = () => {
+  const params = useParams();
+  const [lat, lon] = params;
+
+  const weather = useWeather(lat, lon);
+
+  const songList = useSongList(weather.weatherObj.temperature);
+  console.log(songList);
+
+  return (
+    <div>
+      <Head>
+        <title>Search | Windy</title>
+        <meta
+          name="description"
+          content="Let the weather choose your music for you."
+        />
+      </Head>
+
+      <main>
+        <Banner weatherObj={weather.weatherObj} loading={weather.loading} />
+        <SongList
+          playlistData={songList.playlistData}
+          loading={songList.loading}
+        />
+      </main>
+    </div>
+  );
+};
+
+export default Search;
