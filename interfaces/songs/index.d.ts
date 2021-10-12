614cb84d0aa0372700bef559a4a5c6525e0e8aad
vorsakha <andreitf.dev@gmail.com>
2021-10-12 12:38:06 -0300

    songs types

---
+++ interfaces/songs/index.d.ts
@@ -0,0 +1,13 @@
+interface SongTypes {
+  track: {
+    key: string;
+    title: string;
+    subtitle: string;
+    images: {
+      coverart: string;
+    };
+    share: {
+      snapchat: string;
+    };
+  };
+}
