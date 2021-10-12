9ba597f13bf6df072ce9fe43aed243ab936886f7
vorsakha <andreitf.dev@gmail.com>
2021-10-12 12:34:51 -0300

    alert component

---
+++ components/common/Alert/index.tsx
@@ -0,0 +1,29 @@
+import { useContext, useEffect } from "react";
+import { AlertContext } from "../../../context/AlertContext";
+
+const Alert = () => {
+  const context = useContext(AlertContext);
+
+  useEffect(() => {
+    const clearAlert = setTimeout(() => {
+      context?.setAlert({ msg: null, active: false });
+    }, 1000);
+
+    return () => {
+      clearTimeout(clearAlert);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [context?.alert.active === true]);
+
+  return (
+    <>
+      {context?.alert.active && (
+        <div className="fixed z-50 right-4 top-4 p-1 px-2 text-sm text-gray-200 rounded font-mono font-bold bg-purple-700">
+          {context?.alert.msg}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Alert;
