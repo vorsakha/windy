6a90ca5bdd814d74cb29c672afccb2e542440906
vorsakha <andreitf.dev@gmail.com>
2021-10-12 21:05:38 -0300

    useSongList - add error handling / bugfixes

--- hooks/useSongList.ts
+++ hooks/useSongList.ts
@@ -1,34 +1,42 @@
 import axios from "axios";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { MUSIC_URL } from "../constants";
 
-const useSongList = (temperature: number) => {
+const useSongList = (playlistType: string) => {
   const [playlist, setPlaylist] = useState<any>({
     playlistData: null,
     loading: true,
   });
-
-  const getPlaylist = useCallback(async () => {
-    if (temperature !== undefined) {
-      const response = await axios.get(`${MUSIC_URL}/${temperature}`);
-
-      setPlaylist({ playlistData: response.data, loading: false });
-
-      return;
-    }
-  }, [temperature]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     let mounted = true;
     if (mounted) {
+      const getPlaylist = async () => {
+        if (playlistType !== undefined) {
+          if (playlistType !== "") {
+            try {
+              setError(false);
+              const response = await axios.get(`${MUSIC_URL}/${playlistType}`);
+
+              setPlaylist({ playlistData: response.data, loading: false });
+            } catch (error: any) {
+              console.log(error.message);
+              if (error) setError(true);
+            }
+
+            return;
+          }
+        }
+      };
       getPlaylist();
     }
     mounted = false;
-  }, []);
+  }, [playlistType]);
 
   const { playlistData, loading } = playlist;
 
-  return { playlistData, loading };
+  return { playlistData, loading, error };
 };
 
 export default useSongList;
