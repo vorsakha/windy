dfe6fb9e2ac3f4afcaee52f4af476552b7b1dba1
vorsakha <andreitf.dev@gmail.com>
2021-10-12 21:08:26 -0300

    SongBox - add dynamic key / add fav optionality

--- components/common/SongBox/index.tsx
+++ components/common/SongBox/index.tsx
@@ -3,15 +3,16 @@ import { TemperatureContext } from "../../../context/TemperatureContext";
 import Fav from "../Fav";
 import LoadingSpinner from "../LoadingSpinner";
 
-const SongBox = ({ track }: SongBoxTypes) => {
-  const { images, share, subtitle, title, key } = track;
+const SongBox = ({ track, fav, idx }: SongBoxTypes) => {
+  const { images, share, subtitle, title, key, date } = track;
+  const index = key || idx;
 
   const tempContext = useContext(TemperatureContext);
   const temperature = tempContext?.temperature?.temperature || "";
   const city = tempContext?.temperature?.city || "";
 
   return (
-    <li key={key} className="relative">
+    <li key={index} className="relative">
       {temperature === null || temperature === "" ? (
         <LoadingSpinner />
       ) : (
@@ -20,6 +21,15 @@ const SongBox = ({ track }: SongBoxTypes) => {
         />
       )}
       <img src={images.coverart} alt={title} />
+      {fav ? (
+        <>
+          <p>{temperature}</p>
+          <p>{city}</p>
+          <p>{date}</p>
+        </>
+      ) : (
+        <></>
+      )}
       <h1>{title}</h1>
       <h2>{subtitle}</h2>
       <a href={share.snapchat} target="_blank">
