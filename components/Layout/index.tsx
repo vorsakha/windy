0d22f1e9e137dd4403a2df7dd26f12f7ecf4ab42
vorsakha <andreitf.dev@gmail.com>
2021-10-13 08:57:03 -0300

    Layout - add styles

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,25 +1,38 @@
 import React from "react";
 import Link from "next/link";
 import Alert from "../Alert";
-import SearchBar from "../SearchBar";
+
+import { WiStrongWind as LogoIcon } from "@react-icons/all-files/wi/WiStrongWind";
 
 const Layout: React.FC = ({ children }) => {
   return (
-    <div>
+    <div className="min-w-screen">
       <Alert />
-      <header>
-        <nav>
+      <header className="min-w-screen h-14 px-4">
+        <nav className="w-full flex flex-row justify-between items-center m-auto h-14 max-w-screen-lg border-b border-gray-200">
           <div>
-            <Link href="/">WINDY</Link>
+            <Link href="/">
+              <a className="flex items-center font-bold text-3xl text-blue-400">
+                Windy
+                <LogoIcon className="text-5xl" />
+              </a>
+            </Link>
           </div>
           <div>
-            <Link href="/favs">Favoritas</Link>
-            <SearchBar />
+            <Link href="/">
+              <a className="py-4 mr-6 font-bold hover:text-blue-400">Home</a>
+            </Link>
+            <Link href="/favs">
+              <a className="py-4 font-bold hover:text-blue-400">Favoritas</a>
+            </Link>
           </div>
         </nav>
       </header>
-      {children}
-      <footer>
+
+      <div className="flex flex-col justify-center m-auto max-w-screen-lg min-h-total px-4 z-0">
+        {children}
+      </div>
+      <footer className="flex items-center justify-center w-full h-14">
         <p className="text-center text-gray-500 text-xs">
           &copy;2021 Windy. Todos os direitos reservados.
         </p>
