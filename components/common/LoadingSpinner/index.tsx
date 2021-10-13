862c53fdd05504925377b8355324bc6fe8b38760
vorsakha <andreitf.dev@gmail.com>
2021-10-13 12:12:51 -0300

    LoadingSpinner - color change

--- components/common/LoadingSpinner/index.tsx
+++ components/common/LoadingSpinner/index.tsx
@@ -3,7 +3,7 @@ import { AiOutlineLoading3Quarters as Icon } from "@react-icons/all-files/ai/AiO
 const LoadingSpinner = () => {
   return (
     <div className="flex py-2 px-4 mt-4 no-underline focus:outline-none col-span-2">
-      <Icon className="text-yellow-700 animate-spin text-3xl mx-auto" />
+      <Icon className="text-blue-400 animate-spin text-3xl mx-auto" />
     </div>
   );
 };
