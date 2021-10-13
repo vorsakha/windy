897fabd1a4c8692f1317955d336e8ca2fbeda7f1
vorsakha <andreitf.dev@gmail.com>
2021-10-13 11:12:08 -0300

    pages/index - add localData to home if available

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,28 @@
 import Head from "next/head";
+import { useContext, useEffect } from "react";
 import Banner from "../components/Banner";
 import LoadingSpinner from "../components/common/LoadingSpinner";
 import SongList from "../components/Song/SongList";
+import { LocalDataContext } from "../context/LocalDataContext";
 import useCurrentLocation from "../hooks/useCurrentLocation";
 import useSongList from "../hooks/useSongList";
 import useWeather from "../hooks/useWeather";
 
 const Home = () => {
   const [lat, lon] = useCurrentLocation();
-  const weather = useWeather(lat, lon);
+  const weather = useWeather(lat, lon, true);
 
-  const songList = useSongList(weather.weatherObj.playlistType);
+  const songList = useSongList(weather.weatherObj.playlistType, true);
+
+  const localDataContext = useContext(LocalDataContext);
+  useEffect(() => {
+    weather.weatherObj.city !== "" &&
+      localDataContext?.setLocalWeather(weather.weatherObj);
+  }, [weather]);
+  useEffect(() => {
+    songList.playlistData !== null &&
+      localDataContext?.setLocalSongList(songList.playlistData);
+  }, [songList]);
 
   return (
     <div>
@@ -28,9 +40,14 @@ const Home = () => {
           <LoadingSpinner />
         ) : (
           <>
-            <Banner weatherObj={weather.weatherObj} loading={weather.loading} />
+            <Banner
+              weatherObj={localDataContext?.localWeather || weather.weatherObj}
+              loading={weather.loading}
+            />
             <SongList
-              playlistData={songList.playlistData}
+              playlistData={
+                localDataContext?.localSongList || songList.playlistData
+              }
               loading={songList.loading}
             />
           </>
