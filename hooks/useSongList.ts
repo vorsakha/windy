8b3e090363aa83a1a01cd6842bc7bedf3b240a19
vorsakha <andreitf.dev@gmail.com>
2021-10-13 11:11:08 -0300

    add local data functionality to fetch hooks

--- hooks/useSongList.ts
+++ hooks/useSongList.ts
@@ -1,35 +1,49 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { MUSIC_URL } from "../constants";
+import { LocalDataContext } from "../context/LocalDataContext";
 
-const useSongList = (playlistType: string) => {
+const useSongList = (playlistType: string, home: boolean) => {
   const [playlist, setPlaylist] = useState<any>({
     playlistData: null,
     loading: true,
   });
   const [error, setError] = useState(false);
 
+  const localDataContext = useContext(LocalDataContext);
+
   useEffect(() => {
     let mounted = true;
-    if (mounted) {
-      const getPlaylist = async () => {
-        if (playlistType !== undefined) {
-          if (playlistType !== "") {
-            try {
-              setError(false);
-              const response = await axios.get(`${MUSIC_URL}/${playlistType}`);
+    const getPlaylist = async () => {
+      if (playlistType !== undefined) {
+        if (playlistType !== "") {
+          try {
+            setError(false);
+            const response = await axios.get(`${MUSIC_URL}/${playlistType}`);
 
-              setPlaylist({ playlistData: response.data, loading: false });
-            } catch (error: any) {
-              console.log(error.message);
-              if (error) setError(true);
-            }
-
-            return;
+            setPlaylist({ playlistData: response.data, loading: false });
+          } catch (error: any) {
+            console.log(error.message);
+            if (error) setError(true);
           }
+
+          return;
         }
-      };
-      getPlaylist();
+      }
+    };
+    if (mounted) {
+      if (home && localDataContext?.localSongList === null) {
+        getPlaylist();
+      } else if (!home) {
+        getPlaylist();
+      } else {
+        setPlaylist((prev: any) => {
+          return {
+            ...prev,
+            loading: false,
+          };
+        });
+      }
     }
     mounted = false;
   }, [playlistType]);
