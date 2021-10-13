cad62bf00166ce66ac6fea71dbde531157c495f1
vorsakha <andreitf.dev@gmail.com>
2021-10-12 21:08:43 -0300

    Fav component

---
+++ components/common/Fav/index.tsx
@@ -0,0 +1,35 @@
+import { memo } from "react";
+import useFavIcon from "../../../hooks/useFavIcon";
+import useLocalStorage from "../../../hooks/useLocalStorage";
+import newDate from "../../../utils/newDate";
+
+const Fav = ({
+  items,
+  className = "",
+  big,
+}: {
+  items: LocalStorageTypes;
+  className?: string;
+  big?: boolean;
+}) => {
+  const { icon, handleGetIcon } = useFavIcon(items.key);
+  const { handleSaveToStorage } = useLocalStorage();
+  const date = newDate();
+  const favItems = { ...items, date };
+
+  return (
+    <button
+      className={`${className} ${
+        big ? "static" : "absolute"
+      } right-0 top-0 p-2 text-xl transition-colors hover:text-red-600`}
+      onClick={() => {
+        handleSaveToStorage(favItems);
+        handleGetIcon(items.key);
+      }}
+    >
+      {icon}
+    </button>
+  );
+};
+
+export default memo(Fav);
